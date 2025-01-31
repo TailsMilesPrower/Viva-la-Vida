443ead4d448837f39d765c4b8413b8af1e46a433
Lyuboslav Nalbantov <tails969@gmail.com>
2025-01-31 09:57:52 +0100

    Added Bookshelf stuff

---
+++ Assets/Scripts/KillingBookshelf.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class KillingBookshelf : MonoBehaviour
+{
+    private Rigidbody rb;
+    public Vector3 targetPosition;
+    private bool isPlayerNearby = false;
+    private bool wasTriggered = false;
+    private Collider bookshelfCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+        rb.useGravity = false;
+        bookshelfCollider = GetComponent<Collider>();
+        bookshelfCollider.isTrigger = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerNearby && !wasTriggered && Input.GetKeyDown(KeyCode.F))
+        {
+            wasTriggered = true;
+            ActivateBookshelf();
+        }
+    }
+
+    void ActivateBookshelf()
+    {
+        rb.isKinematic = false;
+        rb.useGravity = true;
+        //bookshelfCollider.isTrigger = false;
+        float forceMagnitude = 5f;
+        Vector3 directionToTarget = (targetPosition - transform.position).normalized;
+
+        Vector3 forceToApply = directionToTarget * forceMagnitude;
+        rb.AddForce(forceToApply, ForceMode.Impulse);
+
+    }
+
+}
