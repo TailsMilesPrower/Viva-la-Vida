140d3c61d6fce868266e459c9f90ffb81b00ec2b
Myre36 <67600948+Myre36@users.noreply.github.com>
2025-02-01 23:38:01 +0100

    Made the damn camera clamp correctly

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -3,28 +3,93 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public float cameraOffsetValue = -5.8f;
+    public GameObject player;
 
-    private float cameraOffsetValueMinimum = -5.8f;
-    private float cameraOffsetValueMaximum = -8.25f;
+    public float smoothSpeed = 5f;
+
+    public Vector3 offset;
+
+    public float cameraZOffsetMin = -5.58f;
+    public float cameraZOffsetMax = -8.25f;
+
+    public Vector3 minCamPosition;
+    public Vector3 maxCamPosition;
 
     private void Update()
     {
-        GetComponent<CinemachineFollow>().FollowOffset.z = cameraOffsetValue;
-
-        if (Input.GetKey(KeyCode.S))
+        if(Input.GetKey(KeyCode.S))
         {
-            if (cameraOffsetValue > cameraOffsetValueMaximum)
+            if(offset.z > cameraZOffsetMax)
             {
-                cameraOffsetValue -= 2f * Time.deltaTime;
+                offset.z -= 2f * Time.deltaTime;
             }
         }
-        else if (Input.GetKey(KeyCode.W))
+        else if(Input.GetKey(KeyCode.W))
         {
-            if (cameraOffsetValue < cameraOffsetValueMinimum)
+            if (offset.z < cameraZOffsetMin)
             {
-                cameraOffsetValue += 2f * Time.deltaTime;
+                offset.z += 2f * Time.deltaTime;
             }
         }
     }
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = player.transform.position + offset;
+
+        Vector3 clampedPosition = new Vector3(Mathf.Clamp(desiredPosition.x, minCamPosition.x, maxCamPosition.x), Mathf.Clamp(desiredPosition.y, minCamPosition.y, maxCamPosition.y), Mathf.Clamp(desiredPosition.z, minCamPosition.z, maxCamPosition.z));
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, clampedPosition, smoothSpeed * Time.deltaTime);
+
+        transform.position = smoothPosition;
+    }
+
+    /*public float cameraMaximumZPosition;
+
+    private float cameraOffsetValueMinimum;
+    private float cameraOffsetValueMaximum;
+
+    private void Update()
+    {
+        if(transform.position.z >= cameraMaximumZPosition)
+        {
+            cameraOffsetValueMinimum = -5.8f;
+            cameraOffsetValueMaximum = -8.25f;
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                if (GetComponent<CinemachineFollow>().FollowOffset.z > cameraOffsetValueMaximum)
+                {
+                    GetComponent<CinemachineFollow>().FollowOffset.z -= 2f * Time.deltaTime;
+                }
+            }
+            else if (Input.GetKey(KeyCode.W))
+            {
+                if (GetComponent<CinemachineFollow>().FollowOffset.z < cameraOffsetValueMinimum)
+                {
+                    GetComponent<CinemachineFollow>().FollowOffset.z += 2f * Time.deltaTime;
+                }
+            }
+        }
+        else
+        {
+            cameraOffsetValueMinimum = -4.96f;
+            cameraOffsetValueMaximum = -8.25f;
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                if (GetComponent<CinemachineFollow>().FollowOffset.z < cameraOffsetValueMinimum)
+                {
+                    GetComponent<CinemachineFollow>().FollowOffset.z += 4f * Time.deltaTime;
+                }
+            }
+            else if (Input.GetKey(KeyCode.W))
+            {
+                if (GetComponent<CinemachineFollow>().FollowOffset.z < cameraOffsetValueMinimum)
+                {
+                    GetComponent<CinemachineFollow>().FollowOffset.z += 2f * Time.deltaTime;
+                }
+            }
+        }
+    }*/
 }
