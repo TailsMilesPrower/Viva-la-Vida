26f4cc16bee3fb1406f03c2799efaeea64e6281c
Myre36 <67600948+Myre36@users.noreply.github.com>
2025-02-03 21:42:04 +0100

    Added a dialouge system

---
+++ Assets/Scripts/DialougeScript.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DialougeScript : MonoBehaviour
+{
+    public GameObject dialougeBox;
+    public TMP_Text dialougeText;
+    public string[] dialouge;
+    public bool playerInRange = false;
+    public int lineNumber;
+
+    public bool outlineEneabled = false;
+
+    private void Start()
+    {
+        dialougeBox = GameObject.Find("DialougeBox");
+        dialougeText = GameObject.Find("DialougeText").GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerInRange)
+        {
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                if(lineNumber < dialouge.Length)
+                {
+                    NextLine();
+                }
+                else
+                {
+                    dialougeBox.GetComponent<RawImage>().enabled = false;
+                    dialougeText.enabled = false;
+                    lineNumber = 0;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            if(outlineEneabled)
+            {
+                GetComponent<Outline>().enabled = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialougeBox.GetComponent<RawImage>().enabled = false;
+            dialougeText.enabled = false;
+            if (outlineEneabled)
+            {
+                GetComponent<Outline>().enabled = false;
+            }
+        }
+    }
+
+    void NextLine()
+    {
+        dialougeBox.GetComponent<RawImage>().enabled = true;
+        dialougeText.enabled = true;
+        dialougeText.text = dialouge[lineNumber];
+        lineNumber++;
+    }
+}
